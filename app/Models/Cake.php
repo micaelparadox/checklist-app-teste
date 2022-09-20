30af453dbee4d301c1c633b167356c7bcaa66d8c
Micael Santana <micaelparadox@gmail.com>
2022-09-20 12:06:39 -0300

    fix: change some stuffs with cake entity

--- app/Models/Cake.php
+++ app/Models/Cake.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Notifications\CakeAnnouncement;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
@@ -17,4 +18,14 @@ class Cake extends Model
         'quantity',
         'email',
     ];
+
+    public function emailNotificationRelation()
+    {
+        return $this->hasMany(CakeAnnouncement::class);
+    }
+
+    public function belongsToCake()
+    {
+        return $this->belongsTo(CakeAnnouncement::class);
+    }
 }
